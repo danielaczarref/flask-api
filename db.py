cdd6c6fb9b9657aaf21269321a58d3755f3cbaa8
danielaczarref <daniela.czarref@gmail.com>
2023-07-09 02:02:08 -0300

    feat (db) creating connection with postgresql local database

--- db.py
+++ db.py
@@ -1,1 +1,21 @@
-business = {}
+from sqlalchemy import create_engine, URL
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+url = URL.create(
+    drivername="postgresql",
+    username="dev",
+    host="localhost",
+    database="bhub",
+    port=5432,
+    password='startrek'
+)
+
+engine = create_engine(url)
+Base = declarative_base()
+conn = engine.connect()
+
+# Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
+session = Session()
+
